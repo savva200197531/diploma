70833e5e584823f7fcbc46ee628fdd89314c9380
savva200197531 <kashin.savva@mail.ru>
2022-04-24 05:07:23 +0700

    Add new packages. Remove firebase hosting auto deploy.

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 import { getStorage } from 'firebase/storage'
+import 'dotenv/config'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
