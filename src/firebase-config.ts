f54dd7d02be84486cad109d277ad082f5f8218d8
savva200197531 <kashin.savva@mail.ru>
2022-04-24 05:17:16 +0700

    Fixes. Remove packages.

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 import { getStorage } from 'firebase/storage'
-import 'dotenv/config'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
